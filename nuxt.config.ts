ca71fcdfae4388e2f0cfa0b08feeff920d30cccf
gorann1 <gorandevop@outlook.com>
2023-01-15 05:56:09 +0100

    dec_package_jsoon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@sidebase/nuxt-auth',
-    '@huntersofbook/naive-ui-nuxt',
-    '@vueuse/nuxt',
-    'unplugin-icons/nuxt',
-    '@intlify/nuxt3',
-    '@pinia/nuxt',
-    '@vueuse/nuxt',
-    'nuxt-windicss'
+    "@sidebase/nuxt-auth",
+    "@huntersofbook/naive-ui-nuxt",
+    "@vueuse/nuxt",
+    "unplugin-icons/nuxt",
+    "@intlify/nuxt3",
+    "@pinia/nuxt",
+    "@vueuse/nuxt",
+    "nuxt-windicss",
   ],
   build: {
-    transpile: [
-      'trpc-nuxt'
-    ]
+    transpile: ["trpc-nuxt"],
   },
   typescript: {
-    shim: false
-  }
-})
+    shim: false,
+  },
+});
