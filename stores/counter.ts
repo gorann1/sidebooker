067a5d88fd9f4887802a444bb2d2fb05a2a4bf65
gorann1 <gorandevop@outlook.com>
2023-01-15 11:07:23 +0100

    Finished_countries

--- stores/counter.ts
+++ stores/counter.ts
@@ -1,25 +1,25 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
 export interface ICounterState {
-  count: number
+  count: number;
 }
 
-export const useCounter = defineStore('counter', {
+export const useCounter = defineStore("counter", {
   state: (): ICounterState => ({
-    count: 0
+    count: 0,
   }),
   actions: {
-    increment () {
-      this.count++
+    increment() {
+      this.count++;
     },
-    decrement () {
-      this.count--
+    decrement() {
+      this.count--;
     },
-    reset () {
-      this.count = 0
+    reset() {
+      this.count = 0;
     },
-    increment2x () {
-      this.count *= 2
-    }
-  }
-})
+    increment2x() {
+      this.count *= 2;
+    },
+  },
+});
