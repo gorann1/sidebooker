8c5dbc2231c40635195f152cd45fde4d2a9949de
gorann1 <gorandevop@outlook.com>
2023-01-18 11:45:21 +0100

    #init_routing

---
+++ pages/countries/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Country details for {{ id }}</p>
+    <div v-for="c in countries" :key="c.id">
+      <p>{ c.description }</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style lang="scss" scoped></style>
